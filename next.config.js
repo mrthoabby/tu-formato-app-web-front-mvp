fc73ce1d247d6e2bf0ea00170c93a45d3ccf745b
Danis Abadia <deanis89@hotmail.com>
2023-04-10 01:44:25 -0500

    feat(redirect) add path structure similar to end product

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/index',
+        destination: '/danis.abadia',
+        permanent: true,
+      },
+    ]
+  },
 }
+
 
 module.exports = nextConfig

ebfa88b151fa850c9d6ca0cc820b26ccc1b2e71a
Danis Abadia <deanis89@hotmail.com>
2023-04-10 01:55:17 -0500

    refactor(domain) domain api scaffolding

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,8 +1,6 @@
-import { User } from "./api/user/DTO/User";
+
 import { TJSXProp } from "./props/TJSXProp";
 
 export type {
-    User as APIUser,
-
     TJSXProp
 }
--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { User } from "../../../domain"
 import user from "../user/user.json"
-export default function handler(req: NextApiRequest, res: NextApiResponse<User>) {
+import { User } from "@/domain/api"
+export default async function handler(req: NextApiRequest, res: NextApiResponse<User>) {
     res.status(200).json(user)
 }
